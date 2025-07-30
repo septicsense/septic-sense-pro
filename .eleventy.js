f1245946dc69ff66c0caf4522ac7039536998360
septicsense <senseseptic@gmail.com>
2025-07-31 05:53:18 +0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function(eleventyConfig) {
-  // Passthrough non-template files
+  // This is the crucial line that fixes all the paths
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy({ "./src/admin/config.yml": "./admin/config.yml" });
 
+  // Add this line to tell Eleventy the base URL of your site
   return {
+    pathPrefix: "/septic-sense-pro/",
     dir: {
       input: "src",
       output: "_site",
